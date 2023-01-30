0748d34f1df432beec899cc72b6a99e2bc00e53d
itsmo1031 <itsmo1031@gmail.com>
2023-01-30 19:00:21 +0900

    ✔️ feat: add solved case

---
+++ BOJ2210.py
@@ -0,0 +1,24 @@
+board = [[*input().split()] for __ in range(5)]
+
+d = [(1, 0), (-1, 0), (0, 1), (0, -1)]
+
+
+def dfs(x, y, num):
+    if len(num) == 6:
+        return answer.add(num)
+
+    for dx, dy in d:
+        nx = x + dx
+        ny = y + dy
+        if 0 <= nx < 5 and 0 <= ny < 5:
+            new_num = num + board[nx][ny]
+            dfs(nx, ny, new_num)
+
+
+answer = set()
+
+for i in range(5):
+    for j in range(5):
+        dfs(i, j, '')
+
+print(len(answer))
