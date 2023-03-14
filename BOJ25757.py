c4619c3fcd2f4c7071a0b06d3b6d78e1c2f65b85
itsmo1031 <itsmo1031@gmail.com>
2023-03-14 17:18:52 +0900

    ✔️ feat: refactoring

--- BOJ25757.py
+++ BOJ25757.py
@@ -4,11 +4,4 @@ player = set()
 for __ in range(int(N)):
     player.add(input())
 
-if G == 'Y':
-    max_p = 1
-elif G == 'F':
-    max_p = 2
-else:
-    max_p = 3
-
-print(len(player) // max_p)
+print(len(player) // "_YFO".index(G))
